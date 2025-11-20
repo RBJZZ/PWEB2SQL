5d6170f2c7d5dc4a0006962c1aaebfcdaca26eb8
DavidAGC2003 <divadagaco2003@gmail.com>
2025-11-20 12:15:45 -0600

    Variables de entorno

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,18 @@
+// 1. IMPORTANTE: Esto debe ir en la primera línea
+require('dotenv').config(); 
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('cup26_database', 'root', 'CrimsonNight16', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+// 2. Sustituimos los textos fijos por las variables
+const sequelize = new Sequelize(
+    process.env.DB_NAME,  // Base de datos
+    process.env.DB_USER,  // Usuario
+    process.env.DB_PASS,  // Contraseña
+    {
+        host: process.env.DB_HOST, // Host (localhost)
+        dialect: 'mysql'           // Dialecto (puedes dejarlo fijo o usar variable)
+    }
+);
 
 async function testConnection() {
     try {
